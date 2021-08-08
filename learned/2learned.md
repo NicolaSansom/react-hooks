d838ec217a45ca60cc6f7fde851224631f8df4eb
Nicola Sansom <nicolasansom1994@gmail.com>
2021-08-09 00:02:55 +0100

    Complete useEffect section

--- learned/2learned.md
+++ learned/2learned.md
@@ -1,6 +1,21 @@
 # useEffect: persistent state
 
-## Extra credit 1
+`React.useEffect` is a built-in hook that allows you to run some custom code
+after React renders (and re-renders) your component to the DOM. It accepts a
+callback function which React will call after the DOM has been updated:
 
-useEffect to set the default state in a more performant way. Custom hoock return
-array. Local storage calls can be come costly use the useState function.
+Function syntax means function is only called on inital state e.g. Only needed
+when there could be a bottleneck as the function is expensive to run.
+
+```js
+const [state, setState] = React.useState(
+  () => window.localStorage.getItem(key) || defaultvalue,
+)
+```
+
+Custom hoock return array. useEffect can be deployed to manage side effects. All
+a custom hook is a is function that uses other hooks inside of it. The `use`
+naming is useful for hooks eslint plugin.
+
+Extra credit 4 only run deserialize if there is something already in local
+storage.
--- src/exercise/02.js
+++ src/exercise/02.js
@@ -20,7 +20,7 @@ import React from 'react'
 //   )
 // }
 
-// Extra credit 1
+// Extra credit 1 & 2
 // function Greeting({initialName = ''}) {
 //   const [name, setName] = React.useState(
 //     () => window.localStorage.getItem('name') || initialName,
@@ -45,19 +45,61 @@ import React from 'react'
 // }
 
 // Extra credit 3
+// const useLocalStorage = (key, defaultvalue = '') => {
+//   const [state, setState] = React.useState(
+//     () => window.localStorage.getItem(key) || defaultvalue,
+//   )
+
+//   React.useEffect(() => {
+//     window.localStorage.setItem(key, state)
+//   }, [key, state])
+
+//   return [state, setState]
+// }
+
+// function Greeting({initialName = ''}) {
+//   const [name, setName] = useLocalStorage('name', initialName)
+
+//   function handleChange(event) {
+//     setName(event.target.value)
+//   }
+//   return (
+//     <div>
+//       <form>
+//         <label htmlFor="name">Name: </label>
+//         <input onChange={handleChange} id="name" />
+//       </form>
+//       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+//     </div>
+//   )
+// }
+
+// Exra credit 4
+const useLocalStorage = (
+  key,
+  defaultvalue = '',
+  {
+    sterilise = value => JSON.stringify(value),
+    desterilise = value => JSON.parse(value),
+  },
+) => {
+  const [state, setState] = React.useState(() => {
+    const valueInLocalStorage = window.localStorage.getItem(key)
+    if (valueInLocalStorage) {
+      return desterilise(valueInLocalStorage)
+    }
+
+    return typeof defaultvalue === 'function' ? defaultvalue() : defaultvalue
+  })
+
+  React.useEffect(() => {
+    window.localStorage.setItem(key, sterilise(state))
+  }, [key, sterilise, state])
+
+  return [state, setState]
+}
+
 function Greeting({initialName = ''}) {
-  const useLocalStorage = (key, defaultvalue = '') => {
-    const [state, setState] = React.useState(() =>
-      window.localStorage.getItem(key),
-    )
-
-    React.useEffect(() => {
-      window.localStorage.setItem(key, state)
-    }, [key, state])
-
-    return [state, setState]
-  }
-
   const [name, setName] = useLocalStorage('name', initialName)
 
   function handleChange(event) {
